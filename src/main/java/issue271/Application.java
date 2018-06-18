a6a26333eba35d3bc497f3115a23086ec405f525
Iván López <lopez.ivan@gmail.com>
2018-06-18 12:25:30 +0200

    Initial commit

---
+++ src/main/java/issue271/Application.java
@@ -0,0 +1,10 @@
+package issue271;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
